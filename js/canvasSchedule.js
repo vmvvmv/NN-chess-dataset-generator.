f4170f56abc52445073889293a124b2ee2cf6f56
Unknown <vmvvmv99@gmail.com>
2018-03-14 16:13:47 +0200

    drawImage function.

--- js/canvasSchedule.js
+++ js/canvasSchedule.js
@@ -4,6 +4,20 @@ function canvasShedule ( id ) {
 
     this.canvas = $( id ).get(0); 
     this.ctx = this.canvas.getContext('2d');
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
 
 }
 
+canvasShedule.prototype.clearRect = function () {
+
+    this.ctx.fillStyle="white";
+    this.ctx.fillRect(0,0,this.width,this.height);
+
+}
+
+canvasShedule.prototype.drawImage = function ( img, x, y ) { 
+
+    this.ctx.drawImage( img,x,y );
+
+}
