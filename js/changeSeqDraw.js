cd96c3f8885fea6d341c82a168f9406c15dc5e86
Unknown <vmvvmv99@gmail.com>
2018-03-14 16:37:10 +0200

    makeDrawState function.

--- js/changeSeqDraw.js
+++ js/changeSeqDraw.js
@@ -1,8 +1,23 @@
 
-function drawSeq ( original, grayscale, randomShuffled  ) {
+function drawSeq ( drawState ) {
 
     this.clearRect();
 
-    this.drawImage(original, 10, 10);
+    var offset = 20;
+
+    var orWidth = drawState.original.width;
+    this.drawImage( drawState.original, orWidth + offset, offset);
+
+    var grWidth = drawState.grayscale.width + orWidth;
+    this.drawImage( drawState.grayscale, grWidth + offset, offset);
+
+    var rsWidth = drawState.randomShuffled.width + grWidth;
+    this.drawImage( drawState.randomShuffled, rsWidth + offset, offset);
+
+}
+
+function makeDrawState ( original, grayscale, randomShuffled ) {
+
+    return { original:original, grayscale:grayscale, randomShuffled: randomShuffled }; 
 
 }
