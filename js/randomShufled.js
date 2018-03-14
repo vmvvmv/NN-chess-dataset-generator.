fbd26f6300c53ddb2dcacbc33c8f56b1832e7c91
Unknown <vmvvmv99@gmail.com>
2018-03-14 17:49:11 +0200

    Test random shuffle image.

--- js/randomShufled.js
+++ js/randomShufled.js
@@ -1,3 +1,43 @@
 // random change image by resize
 
+function RandomShuffled ( canvasShedule ) {
 
+    this.canvasShedule = canvasShedule;
+
+}
+
+RandomShuffled.prototype.shuffle = function ( image ) {
+
+
+    var originalWidth = image.width;
+    var originalHeight = image.height;
+
+    // RANDOM RESIZING
+    var randomResizeWidth = Math.random() * 2000 + 100;
+    var randomResizeHeight = Math.random() * 2000 + 100;
+
+    // range for resizing
+    this.canvasShedule.canvas.width = randomResizeWidth;
+    this.canvasShedule.canvas.height = randomResizeHeight;
+    this.canvasShedule.ctx = this.canvasShedule.canvas.getContext('2d');
+
+    this.canvasShedule.clearRect();
+
+    this.canvasShedule.drawImage(image, 0, 0, randomResizeWidth, randomResizeHeight);
+    
+    var imgPixels = this.canvasShedule.ctx.getImageData(0, 0, randomResizeWidth, randomResizeHeight);
+
+    this.canvasShedule.canvas.width = originalWidth;
+    this.canvasShedule.canvas.height = originalHeight;
+    this.canvasShedule.ctx = this.canvasShedule.canvas.getContext('2d');
+
+    this.canvasShedule.ctx.putImageData(imgPixels, 0, 0, 0, 0, originalWidth, originalHeight);
+
+    var image = new Image();
+    image.src = this.canvasShedule.canvas.toDataURL("image/png", 1);
+
+    this.canvasShedule.clearRect();
+    
+    return image;
+
+}
