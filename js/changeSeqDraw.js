5d4b1c05c04b1f2b6283eac2a46671fe64e5ea85
Unknown <vmvvmv99@gmail.com>
2018-03-14 16:43:01 +0200

    Added labels to drawing images.

--- js/changeSeqDraw.js
+++ js/changeSeqDraw.js
@@ -3,16 +3,22 @@ function drawSeq ( drawState ) {
 
     this.clearRect();
 
-    var offset = 20;
+    var offset = 100;
+
+    this.ctx.font = "17px Arial";
+    this.ctx.fillStyle="black";
 
     var orWidth = drawState.original.width;
-    this.drawImage( drawState.original, orWidth + offset, offset);
+    this.drawImage( drawState.original, 0 + offset, offset);
+    this.ctx.fillText("original",0 + offset, offset / 2 );
 
     var grWidth = drawState.grayscale.width + orWidth;
-    this.drawImage( drawState.grayscale, grWidth + offset, offset);
+    this.drawImage( drawState.grayscale, grWidth + offset , offset);
+    this.ctx.fillText("grayscale", grWidth + offset , offset / 2);
 
     var rsWidth = drawState.randomShuffled.width + grWidth;
-    this.drawImage( drawState.randomShuffled, rsWidth + offset, offset);
+    this.drawImage( drawState.randomShuffled, rsWidth + offset * 2, offset);
+    this.ctx.fillText("random shuffled",rsWidth + offset * 2, offset / 2);
 
 }
 
