78eff0322477d23581e44878b016db830854d7fe
Unknown <vmvvmv99@gmail.com>
2018-03-15 19:19:48 +0200

    Stash

--- generatorServer/public/js/change functions/traningDataGenerator.js
+++ generatorServer/public/js/change functions/traningDataGenerator.js
@@ -13,28 +13,48 @@ TraningDataGenerator.prototype.makeTrainData = function ( image, type ) {
 
     // console.log(this.trainWidth, this.trainHeight);
 
-    this.canvasShedule.canvas.width = this.trainWidth;
-    this.canvasShedule.canvas.height = this.trainHeight;
-    this.canvasShedule.ctx = this.canvasShedule.canvas.getContext('2d');
-    this.canvasShedule.clearRect();
 
-    this.canvasShedule.putData(image.imageSrc, 0, 0,  image.width,  image.height);
 
-    // var imageBitmap = createImageBitmap(image.imageSrc);
-    var image = new Image(this.trainWidth, this.trainHeight);
-
-    image.src =  this.canvasShedule.canvas.toDataURL();
-
-    this.canvasShedule.drawImage(image, image.width,  image.height);
-
-    var imgPixels = this.canvasShedule.ctx.getImageData(0, 0, this.trainWidth, this.trainHeight);
-
-    // console.log(imgPixels);// NEED add type of piece
-    trainingData.push( { imageSrc:imgPixels, type: type} );
+    
 
     this.canvasShedule.canvas.width = image.width;
     this.canvasShedule.canvas.height = image.height;
     this.canvasShedule.ctx = this.canvasShedule.canvas.getContext('2d');
     this.canvasShedule.clearRect();
+    this.canvasShedule.putData(image.imageSrc, 0, 0, image.width, image.height);
+    // debugger;
+
+    var fitImage = new Image();
+    fitImage.src = this.canvasShedule.canvas.toDataURL();
+
+    var that = this;
+    fitImage.onload = function() {
+
+        that.canvasShedule.canvas.width = that.trainWidth;
+        that.canvasShedule.canvas.height = that.trainHeight;
+        that.canvasShedule.ctx = that.canvasShedule.canvas.getContext('2d');
+        that.canvasShedule.clearRect();
+    
+        console.log(fitImage.src);
+    
+        that.canvasShedule.drawImage(fitImage, 0, 0, image.width, image.height);
+    
+        // debugger;
+    
+    
+        var imgPixels = that.canvasShedule.ctx.getImageData(0, 0, that.trainWidth, that.trainHeight);
+    
+        // console.log(imgPixels);// NEED add type of piece
+        trainingData.push( { imageSrc:imgPixels, type: type} );
+    
+        that.canvasShedule.canvas.width = image.width;
+        that.canvasShedule.canvas.height = image.height;
+        that.canvasShedule.ctx = that.canvasShedule.canvas.getContext('2d');
+        that.canvasShedule.clearRect();
+
+    };
+
+
+
 
 }
