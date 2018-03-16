5537fd3611a354754f3cc7c9a8bc2f068729d689
Unknown <vmvvmv99@gmail.com>
2018-03-16 15:57:38 +0200

    generate output function

--- generatorServer/public/js/export/nnJson.js
+++ generatorServer/public/js/export/nnJson.js
@@ -1,3 +1,23 @@
 
 console.log('NN');
 
+//  output issue might be if same piece but different color | first set all labels
+//  output [ BO, BKI, BK, BP, BQ, BR, EC, WKI, WK, WP, WQ, WR  ]
+//  input [ (gray pixel) to 0-1, ..... ] to all images current 25x25 - > 625 input layer
+//  output if BO [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  ]
+
+var exportTrainingSet = [];
+
+var outputLabels =  [ 'BO', 'BKI', 'BK', 'BP', 'BQ', 'BR', 'EC', 'WKI', 'WK', 'WP', 'WQ', 'WR' ];
+
+function generateOutput ( type ) {
+
+    var output = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+    var correctAnswer = outputLabels.indexOf( type );
+
+    output[correctAnswer] = 1;
+
+    return output;
+
+}
